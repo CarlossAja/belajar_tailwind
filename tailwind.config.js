6acb3eeb999d1e08e5e5eda91572c8bdbeeb7093
CarlossAja <carlosdhs12345@gmail.com>
2025-03-21 11:36:59 +0700

    tambahkan files tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#46A358'
+      }
+    },
+  },
+  plugins: [],
+}
+
